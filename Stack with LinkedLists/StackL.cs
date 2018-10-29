89782fda7c82ba9c01821f472a9f7d7fb8ef6da9
Enunwah Stephen <senunwah@yahoo.com>
2018-10-29 08:54:06 +0100

    Implemented Stack with LinkedLists

--- Stack with LinkedLists/StackL.cs
+++ Stack with LinkedLists/StackL.cs
@@ -6,10 +6,78 @@ using System.Threading.Tasks;
 
 namespace Stack_with_LinkedLists
 {
-    class Program
+    class Node
     {
+        public Node next, prev;
+        public int data;
+    }
+    class StackL
+    {
+        Node head;
+        Node tail;
         static void Main(string[] args)
         {
+            StackL sl = new StackL();
+            for (int i = 0; i < 50; i++)
+            {
+                sl.Add(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                sl.Delete(i);
+            }
+
+            sl.Print();
+
+            Console.Read();
+
+        }
+
+        public void Add(int d)
+        {
+            Node tmp = new Node { data = d };
+            tmp.next = head;
+
+            if (head != null)
+                head.prev = tmp;
+            
+                head = tmp;
+
+            if (head.next == null)
+                tail = head;
+            
+        }
+
+        public bool Delete(int n)
+        {
+            Node current = head;
+            Node previous = head;
+            
+            while (current != null)
+            {
+                if (current.data == n)
+                {
+                    previous.next = current.next;
+                    return true;
+                }
+                else
+                {
+                    previous = current;
+                    current = current.next;
+                }
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            Node tmp = head;
+            while(tmp != null)
+            {
+                Console.WriteLine(tmp.data);
+                tmp = tmp.next;
+            }
         }
     }
 }
