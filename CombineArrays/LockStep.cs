36c83b7565d45c5bb6c1f45fcdb279eab4930b12
Enunwah Stephen <senunwah@yahoo.com>
2019-01-04 16:16:30 +0100

    Changes Made

--- CombineArrays/LockStep.cs
+++ CombineArrays/LockStep.cs
@@ -6,41 +6,38 @@ using System.Threading.Tasks;
 
 namespace CombineArrays
 {
-    class Program
+    class LockStep
     {
         
         static void Main(string[] args)
         {
             int[] a = { 1, 2, 3, 4, 5 };
-            int[] b = { 1, 2, 3, 4, 5 };
+            int[] b = { 5,7,7,9,1,4,6,8,9 };
             int index = a.Length + b.Length;
             int[] combinedArray = new int[index];
             CombineArray(a, b, combinedArray);
             printArray(combinedArray);
+            Console.Read();
         }
 
         public static void CombineArray (int[] a, int[] b, int[] combinedArray)
         {
-            int Length;
-            if (a.Length > b.Length)
-                Length = a.Length;
-            else
-                Length = b.Length;
             
-            for (int i = 0; i <= Length; i++)
+            int IdxA = 0, IdxB = 0;
+            for (int x = 0; x < combinedArray.Length;)
             {
-                
-                if(i <= a.Length)
+                if (IdxA < a.Length)
                 {
-                    combinedArray[i] = a[i];
+                    combinedArray[x] = a[IdxA];
+                    x++; IdxA++;
                 }
-                else { }
 
-                
+                if (IdxB < b.Length)
+                {
+                    combinedArray[x] = b[IdxB];
+                    x++; IdxB++;
+                }
             }
-            
-
-
 
         }
 
@@ -48,11 +45,10 @@ namespace CombineArrays
         {
             for (int i = 0; i < combinedArray.Length; i++)
             {
-                Console.Write("{0} ", combinedArray[i]);
+                Console.Write(combinedArray[i]);
             }
 
-            Console.Read();
-
+           
             
         }
     }
---
+++ Rectangle Shaded Aread/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Shaded_Area
+{
+    class Program
+    {
+        int[] Ax = { 2, 4 };
+        int[] Ay = { 2, 6 };
+        int[] Bx = { 3, 5 };
+        int[] By = { 3, 4 };
+
+
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            Console.WriteLine("The Area is {0}", p.FindArea());
+            Console.Read();
+
+        }
+
+        public int FindArea()
+        {
+            int Length = Math.Max(Ax[0], Bx[0]) - Math.Min(Ax[1], Bx[1]);
+            int Breadth = Math.Max(Ay[0], By[0]) - Math.Min(Ay[1], By[1]);
+            int Area = Length * Breadth;
+            return Area;
+        }
+
+    }
+}
